192e18d4c9a9ac423e06c8dc4a5e117b9e911f9a
federicoroldos <fede212yt@gmail.com>
2025-12-04 23:28:42 -0300

    github pages integration with github actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // Needed so assets resolve correctly when served from GitHub Pages.
+  base: '/gamesCompletitionTracker/',
   server: {
     open: true
   }
